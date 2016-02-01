8f0dc391dc0c06f5cd717ec2a7b933d047df47aa
Aden Fraser <aden@jacobbailey.com>
2016-02-01 14:57:27 +0000

    Settings Migration

---
+++ src/Settings/Database/Migrations/2016_02_01_000000_create_flare_settings_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlareSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('setting');
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('settings');
+    }
+}
